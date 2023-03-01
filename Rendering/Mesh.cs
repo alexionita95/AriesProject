a6a516ad40d87f8b74fbfd4baf8b04b22f023b42
alexionita95 <ionita.alexandru95@gmail.com>
2023-03-01 18:46:28 +0200

    Added minimal mesh class

---
+++ Rendering/Mesh.cs
@@ -0,0 +1,68 @@
+﻿using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Rendering
+{
+    public class Mesh
+    {
+        public List<Vector3> Vertices { get; set; }
+        public List<Vector3> Normals { get; set; }
+        public List<Vector2> UVMap { get; set; }
+        public List<uint> Indices { get; set; }
+
+        public int VertexArrayObject;
+        private int VertexBufferObject;
+        private int NormalBufferObject;
+        private int UVBufferObject;
+        private int ElementBufferObject;
+
+
+        public Mesh(List<Vector3> vertices, List<Vector3> normals, List<Vector2> uVMap, List<uint> indices)
+        {
+            Vertices = vertices;
+            Normals = normals;
+            UVMap = uVMap;
+            Indices = indices;
+            Initialize();
+            
+        }
+
+        private void Initialize()
+        {
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+            VertexBufferObject = InitBuffer(BufferTarget.ArrayBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, Vertices.Count * 3 * sizeof(float), Vertices.ToArray(), BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+
+            NormalBufferObject = InitBuffer(BufferTarget.ArrayBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, Normals.Count * 3 * sizeof(float), Normals.ToArray(), BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(1);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+
+            UVBufferObject = InitBuffer(BufferTarget.ArrayBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, UVMap.Count * 2 * sizeof(float), UVMap.ToArray(), BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(2);
+            GL.VertexAttribPointer(2, 3, VertexAttribPointerType.Float, false, 2 * sizeof(float), 0);
+
+            ElementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, Indices.Count * sizeof(uint), Indices.ToArray(), BufferUsageHint.StaticDraw);
+
+        }
+
+        private int InitBuffer(BufferTarget target)
+        {
+            int buffer = GL.GenBuffer();
+            GL.BindBuffer(target, buffer);
+            return buffer;
+        }
+
+    }
+}
--- Rendering/RenderingWindow.cs
+++ Rendering/RenderingWindow.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
 using OpenTK.Graphics.OpenGL4;
@@ -16,9 +17,13 @@ namespace Rendering
         Texture specular;
         Camera camera;
         int VertexBufferObject;
+        int NormalBufferObject;
+        int uvBufferObject;
         int VertexArrayObject;
         int ElementBufferObject;
         uint[] indices;
+
+        Mesh mesh;
         double time = 0;
         private bool firstMove = true;
 
@@ -42,223 +47,143 @@ namespace Rendering
 
             shader = new Shader(@"Resources\Shaders\default.vert", @"Resources\Shaders\material.frag");
             shader.Use();
-            /* float[] vertices =
- {
-     //Position          Texture coordinates
-      0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
-      0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
-     -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
-     -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
- };*/
-            float[] vertices = 
-                
-                
-    /*{
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            List<Vector3> vertices = new List<Vector3>() {
+    // Positions    
+new Vector3(-0.5f, -0.5f, -0.5f),
+new Vector3( 0.5f, -0.5f, -0.5f),
+new Vector3( 0.5f,  0.5f, -0.5f),
+new Vector3( 0.5f,  0.5f, -0.5f),
+new Vector3(-0.5f,  0.5f, -0.5f),
+new Vector3(-0.5f, -0.5f, -0.5f),
 
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+new Vector3(-0.5f, -0.5f,  0.5f),
+new Vector3( 0.5f, -0.5f,  0.5f),
+new Vector3( 0.5f,  0.5f,  0.5f),
+new Vector3( 0.5f,  0.5f,  0.5f),
+new Vector3(-0.5f,  0.5f,  0.5f),
+new Vector3(-0.5f, -0.5f,  0.5f),
 
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+new Vector3(-0.5f,  0.5f,  0.5f),
+new Vector3(-0.5f,  0.5f, -0.5f),
+new Vector3(-0.5f, -0.5f, -0.5f),
+new Vector3(-0.5f, -0.5f, -0.5f),
+new Vector3(-0.5f, -0.5f,  0.5f),
+new Vector3(-0.5f,  0.5f,  0.5f),
 
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-};*/
-    /* {
-            // Position
-            -0.5f, -0.5f, -0.5f, // Front face
-             0.5f, -0.5f, -0.5f,
-             0.5f,  0.5f, -0.5f,
-             0.5f,  0.5f, -0.5f,
-            -0.5f,  0.5f, -0.5f,
-            -0.5f, -0.5f, -0.5f,
+new Vector3( 0.5f,  0.5f,  0.5f),
+new Vector3( 0.5f,  0.5f, -0.5f),
+new Vector3( 0.5f, -0.5f, -0.5f),
+new Vector3( 0.5f, -0.5f, -0.5f),
+new Vector3( 0.5f, -0.5f,  0.5f),
+new Vector3( 0.5f,  0.5f,  0.5f),
 
-            -0.5f, -0.5f,  0.5f, // Back face
-             0.5f, -0.5f,  0.5f,
-             0.5f,  0.5f,  0.5f,
-             0.5f,  0.5f,  0.5f,
-            -0.5f,  0.5f,  0.5f,
-            -0.5f, -0.5f,  0.5f,
+new Vector3(-0.5f, -0.5f, -0.5f),
+new Vector3( 0.5f, -0.5f, -0.5f),
+new Vector3( 0.5f, -0.5f,  0.5f),
+new Vector3( 0.5f, -0.5f,  0.5f),
+new Vector3(-0.5f, -0.5f,  0.5f),
+new Vector3(-0.5f, -0.5f, -0.5f),
 
-            -0.5f,  0.5f,  0.5f, // Left face
-            -0.5f,  0.5f, -0.5f,
-            -0.5f, -0.5f, -0.5f,
-            -0.5f, -0.5f, -0.5f,
-            -0.5f, -0.5f,  0.5f,
-            -0.5f,  0.5f,  0.5f,
-
-             0.5f,  0.5f,  0.5f, // Right face
-             0.5f,  0.5f, -0.5f,
-             0.5f, -0.5f, -0.5f,
-             0.5f, -0.5f, -0.5f,
-             0.5f, -0.5f,  0.5f,
-             0.5f,  0.5f,  0.5f,
-
-            -0.5f, -0.5f, -0.5f, // Bottom face
-             0.5f, -0.5f, -0.5f,
-             0.5f, -0.5f,  0.5f,
-             0.5f, -0.5f,  0.5f,
-            -0.5f, -0.5f,  0.5f,
-            -0.5f, -0.5f, -0.5f,
-
-            -0.5f,  0.5f, -0.5f, // Top face
-             0.5f,  0.5f, -0.5f,
-             0.5f,  0.5f,  0.5f,
-             0.5f,  0.5f,  0.5f,
-            -0.5f,  0.5f,  0.5f,
-            -0.5f,  0.5f, -0.5f
-        };*/
-    /*
-      {
-             // Position          Normal
-            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, // Front face
-             0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-             0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-             0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-            -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-
-            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f, // Back face
-             0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-             0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-             0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-
-            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f, // Left face
-            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-
-             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f, // Right face
-             0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-             0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-             0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-             0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-
-            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f, // Bottom face
-             0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-             0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-             0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-
-            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f, // Top face
-             0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-             0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-             0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-            -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
-        };*/
-
-            {
-                // Positions          Normals              Texture coords
-                -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
-             0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
-             0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
-             0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
-            -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
-
-            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 0.0f,
-             0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 0.0f,
-             0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 1.0f,
-             0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 1.0f,
-            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 1.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 0.0f,
-
-            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
-            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
-            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-
-             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-             0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
-             0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-             0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-             0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
-             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-
-            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
-             0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
-             0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
-             0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
-
-            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
-             0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
-             0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
-             0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
-            -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
-            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+new Vector3(-0.5f,  0.5f, -0.5f),
+new Vector3( 0.5f,  0.5f, -0.5f),
+new Vector3( 0.5f,  0.5f,  0.5f),
+new Vector3( 0.5f,  0.5f,  0.5f),
+new Vector3(-0.5f,  0.5f,  0.5f),
+new Vector3(-0.5f,  0.5f, -0.5f)
         };
 
-            indices = new uint[]{  // note that we start from 0!
-    0, 1, 3   // first triangle
-    ,1, 2, 3    // second triangle
+            List<Vector3> normals = new List<Vector3>{	
+//     Normals        
+ new Vector3(0.0f,  0.0f, -1.0f),
+ new Vector3(0.0f,  0.0f, -1.0f),
+ new Vector3(0.0f,  0.0f, -1.0f),
+ new Vector3(0.0f,  0.0f, -1.0f),
+ new Vector3(0.0f,  0.0f, -1.0f),
+ new Vector3(0.0f,  0.0f, -1.0f),
+
+ new Vector3(0.0f,  0.0f,  1.0f),
+ new Vector3(0.0f,  0.0f,  1.0f),
+ new Vector3(0.0f,  0.0f,  1.0f),
+ new Vector3(0.0f,  0.0f,  1.0f),
+ new Vector3(0.0f,  0.0f,  1.0f),
+ new Vector3(0.0f,  0.0f,  1.0f),
+
+-new Vector3(1.0f,  0.0f,  0.0f),
+-new Vector3(1.0f,  0.0f,  0.0f),
+-new Vector3(1.0f,  0.0f,  0.0f),
+-new Vector3(1.0f,  0.0f,  0.0f),
+-new Vector3(1.0f,  0.0f,  0.0f),
+-new Vector3(1.0f,  0.0f,  0.0f),
+
+ new Vector3(1.0f,  0.0f,  0.0f),
+ new Vector3(1.0f,  0.0f,  0.0f),
+ new Vector3(1.0f,  0.0f,  0.0f),
+ new Vector3(1.0f,  0.0f,  0.0f),
+ new Vector3(1.0f,  0.0f,  0.0f),
+ new Vector3(1.0f,  0.0f,  0.0f),
+
+ new Vector3(0.0f, -1.0f,  0.0f),
+ new Vector3(0.0f, -1.0f,  0.0f),
+ new Vector3(0.0f, -1.0f,  0.0f),
+ new Vector3(0.0f, -1.0f,  0.0f),
+ new Vector3(0.0f, -1.0f,  0.0f),
+ new Vector3(0.0f, -1.0f,  0.0f),
+
+ new Vector3(0.0f,  1.0f,  0.0f),
+ new Vector3(0.0f,  1.0f,  0.0f),
+ new Vector3(0.0f,  1.0f,  0.0f),
+ new Vector3(0.0f,  1.0f,  0.0f),
+ new Vector3(0.0f,  1.0f,  0.0f),
+ new Vector3(0.0f,  1.0f,  0.0f)
+ };
+
+            List<Vector2> uvMap = new List<Vector2>() {
+//      Texture coords
+  new Vector2(0.0f, 0.0f),
+  new Vector2(1.0f, 0.0f),
+  new Vector2(1.0f, 1.0f),
+  new Vector2(1.0f, 1.0f),
+  new Vector2(0.0f, 1.0f),
+  new Vector2(0.0f, 0.0f),
+
+  new Vector2(0.0f, 0.0f),
+  new Vector2(1.0f, 0.0f),
+  new Vector2(1.0f, 1.0f),
+  new Vector2(1.0f, 1.0f),
+  new Vector2(0.0f, 1.0f),
+  new Vector2(0.0f, 0.0f),
+
+  new Vector2(1.0f, 0.0f),
+  new Vector2(1.0f, 1.0f),
+  new Vector2(0.0f, 1.0f),
+  new Vector2(0.0f, 1.0f),
+  new Vector2(0.0f, 0.0f),
+  new Vector2(1.0f, 0.0f),
+
+  new Vector2(1.0f, 0.0f),
+  new Vector2(1.0f, 1.0f),
+  new Vector2(0.0f, 1.0f),
+  new Vector2(0.0f, 1.0f),
+  new Vector2(0.0f, 0.0f),
+  new Vector2(1.0f, 0.0f),
+
+  new Vector2(0.0f, 1.0f),
+  new Vector2(1.0f, 1.0f),
+  new Vector2(1.0f, 0.0f),
+  new Vector2(1.0f, 0.0f),
+  new Vector2(0.0f, 0.0f),
+  new Vector2(0.0f, 1.0f),
+
+  new Vector2(0.0f, 1.0f),
+  new Vector2(1.0f, 1.0f),
+  new Vector2(1.0f, 0.0f),
+  new Vector2(1.0f, 0.0f),
+  new Vector2(0.0f, 0.0f),
+  new Vector2(0.0f, 1.0f)
 };
-
-
-            VertexArrayObject = GL.GenVertexArray();
-            GL.BindVertexArray(VertexArrayObject);
-
-
-            VertexBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-
-            ElementBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
-
-            GL.EnableVertexAttribArray(0);
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 0);
-            GL.EnableVertexAttribArray(1);
-            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 3 * sizeof(float));
-
-            GL.EnableVertexAttribArray(2);
-            GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), 6 * sizeof(float));
-
-            //  GL.EnableVertexAttribArray(1);
-            // GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
+            mesh = new Mesh(vertices,normals,uvMap,new List<uint>());
             texture = Texture.LoadFromFile(@"Resources\Textures\Character.png");
             specular = Texture.LoadFromFile(@"Resources\Textures\Character_specular.png");
 
@@ -279,7 +204,7 @@ namespace Rendering
             GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             
-            GL.BindVertexArray(VertexArrayObject);
+            GL.BindVertexArray(mesh.VertexArrayObject);
             texture.Use();
             specular.Use(TextureUnit.Texture1);
             shader.Use();
